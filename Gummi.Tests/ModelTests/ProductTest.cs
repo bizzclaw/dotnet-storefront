8de91f7881e3868e79be703e30c4aa04589180a8
Joseph Tomlinson <bizzclaw1@gmail.com>
2018-01-14 12:45:26 -0800

    add test project

---
+++ Gummi.Tests/ModelTests/ProductTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gummi.Tests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
